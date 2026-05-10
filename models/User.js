a7f5ac10d0e9b9be1d0acde2135b61a9bfc5d063
Archit <143813492+archit2400@users.noreply.github.com>
2026-05-10 15:42:16 +0530

    Updated gitignore

--- models/User.js
+++ models/User.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["patient", "doctor"], default: "patient" }
+  name: { type: String, 
+    required: true },
+  email: { type: String,
+     required: true, 
+     unique: true },
+  password: { type: String,
+     required: true },
+  role: { type: String,
+     enum: ["patient", "doctor"],
+      default: "patient" }
 });
 
 export default mongoose.model("User", userSchema);
